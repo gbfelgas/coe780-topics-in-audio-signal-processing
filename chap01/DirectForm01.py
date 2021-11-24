9f62902813c2745ed0d66bfc4191881ce9689acd
gbfelgas <gbfelgas@gmail.com>
2021-11-24 15:14:53 -0200

    chore: save DirectForm01 plots in figures folder

--- chap01/DirectForm01.py
+++ chap01/DirectForm01.py
@@ -1,13 +1,5 @@
-# Author: U. Zölzer (Matlab)
 # Impulse response of 2nd order IIR filter
 # Sample-by-sample algorithm
-#
-#--------------------------------------------------------------------------
-# This source code is provided without any warranties as published in 
-# DAFX book 2nd edition, copyright Wiley & Sons 2011, available at 
-# http://www.dafx.de. It may be used for educational purposes and not 
-# for commercial applications without further permission.
-#--------------------------------------------------------------------------
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -18,8 +10,8 @@ a=np.array([1, -1.28, 0.47])
 b=np.array([0.69, -1.38, 0.69])
 
 # Initialization of state variables
-xh1=0;xh2=0;
-yh1=0;yh2=0;
+xh1=0;xh2=0
+yh1=0;yh2=0
 
 # Input signal: unit impulse
 N=20;  # length of input signal
@@ -29,9 +21,9 @@ x[0]=1
 # Sample-by-sample algorithm
 y = np.zeros(N)
 for n in range(N):
-    y[n]=b[0]*x[n] + b[1]*xh1 + b[2]*xh2 - a[1]*yh1 - a[2]*yh2;
-    xh2=xh1;xh1=x[n];
-    yh2=yh1;yh1=y[n];
+    y[n]=b[0]*x[n] + b[1]*xh1 + b[2]*xh2 - a[1]*yh1 - a[2]*yh2
+    xh2=xh1;xh1=x[n]
+    yh2=yh1;yh1=y[n]
 
 
 # Plot results
@@ -54,5 +46,5 @@ ax.xaxis.set_major_formatter('{x:.0f}')
 ax.xaxis.set_major_locator(MultipleLocator(5))
 plt.ylabel('y(n) \u2192')
 plt.subplots_adjust(hspace=0.4)
-plt.show()
 
+plt.savefig("figures/DirectForm01.png", dpi=300, bbox_inches='tight')
