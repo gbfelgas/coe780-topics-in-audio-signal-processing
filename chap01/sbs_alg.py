bb0d9f41435a99542289e8b928d0f1c8cb9cf011
gbfelgas <gbfelgas@gmail.com>
2021-11-24 14:53:41 -0200

    chore: remove initial comments and add audio output to sbs_alg

--- chap01/sbs_alg.py
+++ chap01/sbs_alg.py
@@ -1,26 +1,17 @@
-# Author: U. Zölzer (Matlab)
-#
-#--------------------------------------------------------------------------
-# This source code is provided without any warranties as published in 
-# DAFX book 2nd edition, copyright Wiley & Sons 2011, available at 
-# http://www.dafx.de. It may be used for educational purposes and not 
-# for commercial applications without further permission.
-#--------------------------------------------------------------------------
 import audiofile as af
-import numpy as np
 
 #Parameters
-inputFile = 'ton2.wav'       #input filename
-outputFile = 'ton2_out.wav'  #output filename
+input_file = 'audios/ton2.wav'       #input filename
+output_file = 'audios/ton2_out.wav'  #output filename
 a = 2
 Nbits = 16
 
 # Read input sound file into vector x(n) and sampling frequency FS
-x, FS = af.read(inputFile)
+x, FS = af.read(input_file)
 
 # Sample-by sample algorithm y(n)=a*x(n)
-y = a * x 
+y = a * x
 
-# Write y(n) into output sound file with number of 
+# Write y(n) into output sound file with number of
 # bits Nbits and sampling frequency FS
-af.write(outputFile,y,FS,Nbits)
+af.write(output_file, y, FS, Nbits)
