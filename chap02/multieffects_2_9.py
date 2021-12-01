aab2895f38da5d9cdae00555739f7b34234823a9
daniloolivieri <94622553+daniloolivieri@users.noreply.github.com>
2021-12-01 13:32:08 -0300

    Update and rename multieffects_2-9.py to multieffects_2_9.py

--- chap02/multieffects_2_9.py
+++ chap02/multieffects_2_9.py
@@ -1,11 +1,3 @@
-# Author: S. Disch
-#
-#--------------------------------------------------------------------------
-# This source code is provided without any warranties as published in 
-# DAFX book 2nd edition, copyright Wiley & Sons 2011, available at 
-# http://www.dafx.de. It may be used for educational purposes and not 
-# for commercial applications without further permission.
-#--------------------------------------------------------------------------
 import numpy as np
 import scipy.signal as sig
 
