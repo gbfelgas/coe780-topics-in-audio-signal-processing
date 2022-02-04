59ebad42dd9d0344cb9dd6435b57fdb1a188bebd
daniloolivieri <94622553+daniloolivieri@users.noreply.github.com>
2022-02-04 11:19:54 -0300

    Update UX_cross_synthesis_LPC.py

--- chap08/UX_cross_synthesis_LPC.py
+++ chap08/UX_cross_synthesis_LPC.py
@@ -15,8 +15,8 @@ import math
 from calc_lpc import *
 
  #----- user data -----
-DAFx_in_sou, FS = librosa.load('moore_guitar.wav')   # sound 1: source/excitation
-DAFx_in_env, FS   = librosa.load('toms_diner.wav')         # sound 2: spectral env.
+DAFx_in_sou, FS = sf.read('moore_guitar.wav')   # sound 1: source/excitation
+DAFx_in_env, FS = sf.read('toms_diner.wav')         # sound 2: spectral env.
 long          = 400         # block len for calculation of coefficients
 hopsize       = 160         # hop size (is 160)
 env_order     = 20           # order of the LPC for source signal
@@ -38,7 +38,7 @@ DAFx_in_env = DAFx_in_env / max(abs(DAFx_in_env))
 
 DAFx_out = np.zeros(ly)      # result sound
 exc      = np.zeros(ly)      # excitation sound
-w        = np.hanning(long)    # window
+w        = np.hanning(long+1)[:long]    # window, 'periodic'
 N_frames = math.floor((ly-env_order-long)/hopsize)  # number of frames
 
  #----- Perform ross-synthesis -----
@@ -51,15 +51,16 @@ for j in range(N_frames):
   A_env, g_env  = calc_lpc(DAFx_in_env[k :k+long]*w, env_order)
   A_sou, g_sou  = calc_lpc(DAFx_in_sou[k: k+long]*w, source_order)
   gain[j] = g_env
-  ae      = - A_env[0:env_order]  # LPC coeff. of excitation
+  ae      = - A_env[1:env_order+1]  # LPC coeff. of excitation
   for n in range(hopsize):
-    excitation1   = (A_sou/g_sou) @ DAFx_in_sou[k+n+1:k+n-source_order:-1]
-    exc[k+n-1]      = np.array(excitation1)
-    DAFx_out[k+n-1] = ae @ DAFx_in_sou[k+n:k+n-env_order:-1] + g_env*excitation1
+    excitation1   = (A_sou/g_sou) @ DAFx_in_sou[k+n:k+n-source_order-1:-1]
+    exc[k+n]      = np.array(excitation1)
+    auxDAFx_out = DAFx_out[k+n-env_order:k+n]
+    DAFx_out[k+n] = ae @ auxDAFx_out[-1::-1] + g_env*excitation1
 #toc
 
  #----- playing and saving output signal -----
-DAFx_out      = DAFx_out[env_order:len(DAFx_out)-1] / max(abs(DAFx_out))
+DAFx_out      = DAFx_out[env_order:] / max(abs(DAFx_out))
 #soundsc(DAFx_out, FS)
 DAFx_out_norm = r * DAFx_out/max(abs(DAFx_out))  # scale for wav output
 sf.write('CrossLPC.wav', DAFx_out_norm, FS)
